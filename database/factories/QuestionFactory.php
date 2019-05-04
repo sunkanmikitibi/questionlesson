31b6e07fae503f6dd7e8faa70a88e1e0d770f2f5
Sunkanmi Olukitibi <sunkanmikitibi@gmail.com>
2019-05-04 10:32:53 +0100

    first commit

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5, 10)), ".")
+    ];
+});
